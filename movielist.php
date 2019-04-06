3d2643d2378dcbdea440713bfdfa5ee0416ca9fb
danielyoo98 <43011091+danielyoo98@users.noreply.github.com>
2019-04-05 22:41:56 -0700

    Updated movielist.php

--- movielist.php
+++ movielist.php
@@ -49,63 +49,31 @@
 	<div class="container">
 		<div class="info">
             <h3>All Movies</h3>
-            <div class="jumbotron scrollbox">
-                <div class="movie">
-                    <img class="float-left" src="images/avengers.jpg">
-                    <h4 class="float-right">Avengers Infinity War</h4>
-					<p class="float-right" id="director">Director: Russo Brothers</p>
-					<p class="float-right" id="description">Genre: Action</p>
-                    <p class="float-right" id="description">Description: The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe</p>
-                </div>
-                <div class="movie">
-                    <img class="float-left" src="images/ironman.jpg">
-                    <h4 class="float-right">Iron Man</h4>
-					<p class="float-right" id="director">Director: Jon Favreau</p>
-					<p class="float-right" id="description">Genre: Action</p>
-                    <p class="float-right" id="description">Description: The film follows Tony Stark (Robert Downey Jr.), an industrialist and master engineer who builds a powered exoskeleton after a life-threatening incident, and becomes the technologically advanced superhero Iron Man.</p>
-				</div>
-				<div class="movie">
-					<img class="float-left" src="images/grudge.jpg">
-					<h4 class="float-right">The Grudge</h4>
-					<p class="float-right" id="director">Director: Takashi Shimizu</p>
-					<p class="float-right" id="description">Genre: Mystery/Thriller</p>
-					<p class="float-right" id="description">Plot: Matthew Williams (William Mapother), his wife, Jennifer (Clea DuVall), and mother, Emma (Grace Zabriskie), are Americans making a new life in Tokyo.</p>
-				</div>
-				<div class="movie">
-					<img class="float-left" src="images/para.jpg">
-					<h4 class="float-right">Paranormal Activity</h4>
-					<p class="float-right" id="director">Director: Oren Peli</p>
-					<p class="float-right" id="description">Genre: Thriller/Indie</p>
-					<p class="float-right" id="description">Plot: Soon after moving into a suburban tract home, Katie (Katie Featherston) and Micah (Micah Sloat) become increasingly disturbed by what appears to be a supernatural presence.</p>
-				</div>
-				<div class="movie">
-					<img class="float-left" src="images/tazza.jpg">
-					<h4 class="float-right">Tazza: The Hidden Card</h4>
-					<p class="float-right" id="director">Director: Kang Hyeong-cheol</p>
-					<p class="float-right" id="description">Genre: Drama/Crime</p>
-					<p class="float-right" id="description">Plot: After Ham Dae-gil enters the gambling world, he is set up as the fall guy in a crooked deal. To get revenge, he enters a final game which could be fatal to the loser.</p>
-				</div>
-				<div class="movie">
-					<img class="float-left" src="images/out.jpeg">
-					<h4 class="float-right">The Outlaws</h4>
-					<p class="float-right" id="director">Director: Kang Yoon-Sung</p>
-					<p class="float-right" id="description">Genre: Crime/Thriller</p>
-					<p class="float-right" id="description">Plot: Based on real events that occurred in 2007 dubbed the "Heuksapa Incident", the story follows a turf war that grows between a local gang that runs Garibong-dong in Guro District, Seoul.</p>
-				</div>
-                <div class="movie">
-                    <img class="float-left" src="images/guardians.jpg">
-                    <h4 class="float-right">Guardians Of The Galaxy</h4>
-					<p class="float-right" id="director">Director: James Gunn</p>
-					<p class="float-right" id="description">Genre: Action</p>
-                    <p class="float-right" id="description">Description: To help fight Ronan and his team and save the galaxy from his power, Quill creates a team of space heroes known as the "Guardians of the Galaxy" to save the galaxy.</p>
-                </div>
-                <div class="movie">
-                    <img class="float-left" src="images/thor.jpg">
-                    <h4 class="float-right">Thor Ragnarok</h4>
-					<p class="float-right" id="director">Director: Taika Waititi</p>
-					<p class="float-right" id="description">Genre: Action</p>
-                    <p class="float-right" id="description">Description: Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his home-world and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.</p>
-                </div>
+            <div class="jumbotron scrollbox" id="lists">
+                <?php
+                $result = $db->query("SELECT movie_image_file, movie_title, movie_description FROM movies");
+                $movies = array();
+                echo mysqli_num_rows($result);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $movie = new stdClass();
+                        $movie->imgPath = $row["movie_image_file"];
+                        $movie->title = $row["movie_title"];
+                        $movie->genre = "Action";
+                        $movie->description = $row["movie_description"];
+                        array_push($movies, $movie);
+                    }
+                    foreach ($movies as $movie) { ?>
+                        <div class="movie">
+                        <img class="float-left" src="<?php echo $movie->imgPath ?>">
+                        <h4 class="float-right"><?php echo $movie->title ?></h4>
+                        <p class="float-right" id="director"><?php echo $movie->director ?></p>
+                        <p class="float-right" id="description">Genre: <?php echo $movie->genre ?></p>
+                        <p class="float-right" id="description">Description: <?php echo $movie->description ?></p>
+                        </div>
+                <?php }
+                } 
+                ?>
             </div>
     	</div>
 	</div>
